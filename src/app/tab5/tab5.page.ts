e8525c1cfa389c8b4049f4cb59c048eb9aa87858
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-16 21:26:14 +0800

    finilizing the pages

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,16 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
+  IonIcon,
+  AlertController,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  cafeOutline,
+  phonePortraitOutline,
+  heartOutline,
+  rocketOutline,
+  starOutline,
+  flashOutline,
+  cubeOutline,
+  locateOutline,
+  giftOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
-export class Tab5Page {}
+export class Tab5Page {
+  constructor(private alertController: AlertController) {
+    addIcons({
+      cafeOutline,
+      phonePortraitOutline,
+      heartOutline,
+      rocketOutline,
+      starOutline,
+      flashOutline,
+      cubeOutline,
+      locateOutline,
+      giftOutline,
+    });
+  }
+
+  async showAlert(title: string, message: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: message,
+      buttons: ['OK'],
+      cssClass: 'custom-alert',
+    });
+
+    await alert.present();
+  }
+}
